8a1362c72d3f3b9c2610dc057e24f5c3d8211d27
Obinna Okpokwasili <limitless@Obinnas-MacBook-Pro-2.local>
2018-09-05 15:32:31 -0500

    using different modules, adding inputs

--- main.tf
+++ main.tf
@@ -2,3 +2,22 @@ module "ec2-instance" {
   source  = "app.terraform.io/obicarl/ec2-instance/aws"
   version = "0.0.1"
 }
+
+module "ec2_cluster" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name           = "my-cluster"
+  instance_count = 5
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
