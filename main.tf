287ca2bdb8e8a63a07d4864d768d2293ba75670f
Obinna Okpokwasili <limitless@Obinnas-MacBook-Pro-2.local>
2018-09-06 12:50:14 -0500

    testing auto apply

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 
-module "ec2_instance" {
-  source  = "app.terraform.io/obicarl/ec2-instance/aws"
-  version = "0.0.1"
-
-  ami = "ami-ebd02392"
-  instance_type = "t2.micro"
-  name = "obinna"
-  subnet_id = "subnet-eddcdzz4"
-  vpc_security_group_ids = "sg-12345678"
-}
-
-module "ec2-instance" {
-  source  = "app.terraform.io/obicarl/ec2-instance/aws"
-  version = "0.0.1"
+module "now" {
+  source  = "app.terraform.io/obicarl/now/testing"
+  version = "0.0.2"
 }
